66023d465cf7e23f57bfc2797dc3f22196bbdde1
ruiming <ruiming.zhuang@gmail.com>
2017-06-03 15:16:44 +0800

    fix(index): fix wrong fromSrc when called in outer

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const path = require('path')
 const requireDirectory = require('./lib/require-directory')
 
-const { lib } = requireDirectory('./lib/**/*.js')
+const { lib } = requireDirectory('./lib/**/*.js', {
+  root: path.resolve(__dirname, '..')
+})
 
 module.exports = lib
